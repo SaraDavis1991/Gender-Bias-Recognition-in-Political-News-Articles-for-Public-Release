820127344095d7b24086a539c31cfc31062559a1
Cody Worsnop <codyworsnop@unr.edu>
2019-11-03 11:55:51 -0800

    updated article json syntax

--- Orchestrator.py
+++ Orchestrator.py
@@ -5,5 +5,5 @@ import ApplicationConstants
 reader = DataReader()
 sources = reader.Load(ApplicationConstants.all_articles)
 
-articles = sources[ApplicationConstants.Fox]
-a = 5 
+#need to pull nyt when fixed
+articles = sources[ApplicationConstants.Fox] + sources[ApplicationConstants.usa_today] + sources[ApplicationConstants.Breitbart] + sources[ApplicationConstants.HuffPost]
