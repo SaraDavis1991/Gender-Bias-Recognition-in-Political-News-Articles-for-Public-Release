6fb70e4043117ee1b93e929f2a6026460f767550
Cody Worsnop <codyworsnop@unr.edu>
2019-11-16 12:45:21 -0800

    orchestrator updates

--- Orchestrator.py
+++ Orchestrator.py
@@ -17,18 +17,24 @@ class Orchestrator():
     def read_data(self):       
         return self.Reader.Load_Splits(ApplicationConstants.all_articles)
 
-    def clean_all(self, data):
-        contents = []
-        for article in data:
-            content = article.Content
-            cleaned_content = orchestrator.Preprocessor.Clean(content)
-            contents.append(cleaned_content)
+    def clean_all(self, splits):
+
+        for key in splits:
+
+            data = splits[key]
+            contents = []
+            
+            for article in data:
+
+                content = article.Content
+                cleaned_content = orchestrator.Preprocessor.Clean(content)
+                contents.append((cleaned_content, article.Label.TargetGender))
 
         return contents
     
-    def embed_fold(self, data):
+    def embed_fold(self, cleaned_with_labels):
         
-        targets, regressors = self.docEmbed.Embed(data)
+        targets, regressors = self.docEmbed.Embed(cleaned_with_labels)
         print(targets)
 
         return targets
@@ -36,9 +42,9 @@ class Orchestrator():
 
 
 orchestrator = Orchestrator()
-data = orchestrator.read_data() 
-contents = orchestrator.clean_all(data)
-embed = orchestrator.embed_fold(contents)
+splits = orchestrator.read_data() 
+cleaned_with_labels = orchestrator.clean_all(splits)
+embed = orchestrator.embed_fold(cleaned_with_labels)
 
 
 print (contents)
