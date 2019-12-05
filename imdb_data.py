ca17c92e5ac1fea9cdb11f49dc4d3c504734b4be
Cody Worsnop <cody@worsnop.com>
2019-12-04 21:54:15 -0800

    inferring sents

--- imdb_data.py
+++ imdb_data.py
@@ -24,8 +24,11 @@ from Models.NN_engine import NN
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
 from gensim.models import Doc2Vec
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+from nltk.tokenize import word_tokenize
 # numpy
-import numpy
+import numpy 
 # shuffle
 from random import shuffle
 
@@ -39,6 +42,10 @@ logger = logging.getLogger(program)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
 logging.root.setLevel(level=logging.INFO)
 logger.info("running %s" % ' '.join(sys.argv))
+import os.path
+
+imdb_sentiment_path = "./sentiment/imdb_sentiment.npy"
+imdb_sentiment_label_path = "./sentiment/imdb_sentiment_labels.npy"
 
 class LabeledLineSentence(object):
 	def __init__(self, sources):
@@ -90,8 +97,21 @@ class LabeledLineSentence(object):
 				labels[index] = 0
 			elif "POS" in label:
 				labels[index] = 1
+	
+		tagged_doc_articles = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[labels[i]]) for i, _d in enumerate(words)]
+		
+		if (not os.path.isfile(imdb_sentiment_label_path or imdb_sentiment_path)):
+			
+			targets, feature_vectors = zip(*[(doc.tags[0], model.infer_vector(doc.words, steps=20)) for doc in tagged_doc_articles])
+		
 
-		imdbVectors = model.infer_vector(words)
+			numpy.save(imdb_sentiment_path, feature_vectors)
+			numpy.save(imdb_sentiment_label_path, targets)
+		else:
+
+			targets = numpy.load(imdb_sentiment_label_path) 
+			feature_vectors = numpy.load(imdb_sentiment_path)
+
 		return imdbVectors, labels
 
 	
