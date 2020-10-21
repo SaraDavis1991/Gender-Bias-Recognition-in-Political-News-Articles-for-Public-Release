94635412e6d148de4f21244e33fb060af1c72161
sarad <sarad@nevada.unr.edu>
2020-10-21 14:23:16 -0700

    fixed run_preprocessor.py

--- run_preprocessor.py
+++ run_preprocessor.py
@@ -6,10 +6,11 @@
 from Orchestrator import *
 import ApplicationConstants
 from parse_sentences import run
+from DataReader import *
 
 orchestrator = Orchestrator()
+reader = DataReader()
 
-#We only randomize the very first time we run this; if you do not run the first read_data below, you should set random = true in the next read
 
 #Randomize the articles that were scraped from the links (UPDATE THIS ONCE WE HAVE A FILE WITH LINKS)
 orchestrator.read_data(path = "Data/articles_v3.json", save=True,
@@ -17,7 +18,8 @@ orchestrator.read_data(path = "Data/articles_v3.json", save=True,
 
 #Clean the randomized articles
 orchestrator.read_data(path=ApplicationConstants.all_articles_random_v4, save=True,
-                       savePath=ApplicationConstants.all_articles_random_v4_cleaned, random=False, clean=True, number_of_articles=1000)
+                       savePath=ApplicationConstants.all_articles_random_v4_cleaned, random=True, clean=True, number_of_articles=1000)
+
 
 #Create the candidate name sentence json using parse_sentences and the randomized (dirty) articles
 run()
@@ -25,9 +27,9 @@ run()
 #Clean the dirty sentences containing the candidate name, then do POS tagging, save ADJ
 orchestrator.read_data(path=ApplicationConstants.all_articles_random_v4_candidate_names, save=True,
                        savePath=ApplicationConstants.all_articles_random_v4_cleaned_pos_candidate_names, clean=True,
-                        random=False, number_of_articles=1000, pos_tagged=True)
+                        random=True, number_of_articles=1000, pos_tagged=True)
 
 #Clean all of the sentences (regardless of if candidate name is there), then do POS tagging, save ADJ
 orchestrator.read_data(path=ApplicationConstants.all_articles_random_v4, save=True,
                        savePath=ApplicationConstants.all_articles_random_v4_cleaned_pos, clean=True,
-                        random=False, number_of_articles = 1000, pos_tagged=True)
+                        random=True, number_of_articles = 1000, pos_tagged=True)
